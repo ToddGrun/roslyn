25922058e57b66e129e8517d8ba51f5024c7bb2a
Todd Grunke <toddgrun@microsoft.com>
2023-09-27 11:50:00 -0700

    temp commit

--- src/Compilers/CSharp/Portable/Symbols/Metadata/PE/PENamespaceSymbol.cs
+++ src/Compilers/CSharp/Portable/Symbols/Metadata/PE/PENamespaceSymbol.cs
@@ -2,20 +2,190 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
 
 using Microsoft.CodeAnalysis.PooledObjects;
 using Roslyn.Utilities;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace Microsoft.CodeAnalysis.CSharp.Symbols.Metadata.PE
 {
+    internal struct FrugalDictionary<TKey, TValue> where TKey : notnull
+    {
+        private const int InlineCapacity = 4;
+
+        private readonly IEqualityComparer<TKey> _comparer;
+
+        private KeyValuePair<TKey, TValue> _item0;
+        private KeyValuePair<TKey, TValue> _item1;
+        private KeyValuePair<TKey, TValue> _item2;
+        private KeyValuePair<TKey, TValue> _item3;
+
+        private Dictionary<TKey, TValue>? _dictionary;
+
+        private int _count;
+
+        public FrugalDictionary(IEqualityComparer<TKey>? comparer = null)
+        {
+            _comparer = comparer ?? EqualityComparer<TKey>.Default;
+        }
+
+        public static FrugalDictionary<TKey, TValue> Empty => default;
+
+        public void Add(TKey key, TValue value)
+        {
+            if (_dictionary is not null)
+            {
+                _dictionary.Add(key, value);
+                return;
+            }
+
+            _count++;
+            switch (_count)
+            {
+                case 1:
+                    _item0 = new KeyValuePair<TKey, TValue>(key, value);
+                    break;
+                case 2:
+                    _item1 = new KeyValuePair<TKey, TValue>(key, value);
+                    break;
+                case 3:
+                    _item2 = new KeyValuePair<TKey, TValue>(key, value);
+                    break;
+                case 4:
+                    _item3 = new KeyValuePair<TKey, TValue>(key, value);
+                    break;
+                default:
+                    MoveInlineToDictionary();
+                    break;
+            };
+        }
+
+        [MemberNotNull(nameof(_dictionary))]
+        private void MoveInlineToDictionary()
+        {
+            Debug.Assert(_dictionary is null);
+
+            var dictionary = new Dictionary<TKey, TValue>();
+
+            dictionary.Add(_item0.Key, _item0.Value);
+            dictionary.Add(_item1.Key, _item1.Value);
+            dictionary.Add(_item2.Key, _item2.Value);
+            dictionary.Add(_item3.Key, _item3.Value);
+
+#if NETCOREAPP
+            if (RuntimeHelpers.IsReferenceOrContainsReferences<KeyValuePair<TKey, TValue>>())
+#endif
+            {
+                _item0 = default!;
+                _item1 = default!;
+                _item2 = default!;
+                _item3 = default!;
+            }
+
+            _count = 0;
+            _dictionary = dictionary;
+        }
+
+        public readonly int Count => _dictionary?.Count ?? _count;
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue? value)
+        {
+            if (_dictionary is not null)
+                return _dictionary.TryGetValue(key, out value);
+
+            if (_count == 0)
+            {
+                value = default;
+                return false;
+            }
+
+            if (_comparer.Equals(key, _item0.Key))
+            {
+                value = _item0.Value;
+                return true;
+            }
+
+            if (_count == 1)
+            {
+                value = default;
+                return false;
+            }
+
+            if (_comparer.Equals(key, _item1.Key))
+            {
+                value = _item1.Value;
+                return true;
+            }
+
+            if (_count == 2)
+            {
+                value = default;
+                return false;
+            }
+
+            if (_comparer.Equals(key, _item2.Key))
+            {
+                value = _item2.Value;
+                return true;
+            }
+
+            if (_count == 3)
+            {
+                value = default;
+                return false;
+            }
+
+            if (_comparer.Equals(key, _item3.Key))
+            {
+                value = _item3.Value;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void AddValuesToArrayBuilder(ArrayBuilder<TValue> values)
+        {
+            if (_dictionary is not null)
+            {
+                foreach (KeyValuePair<TKey, TValue> kvp in _dictionary)
+                {
+                    values.Add(kvp.Value);
+                }
+                return;
+            }
+
+            if (_count == 0)
+                return;
+
+            values.Add(_item0.Value);
+
+            if (_count == 1)
+                return;
+
+            values.Add(_item1.Value);
+
+            if (_count == 2)
+                return;
+
+            values.Add(_item2.Value);
+
+            if (_count == 3)
+                return;
+
+            values.Add(_item3.Value);
+        }
+    }
+
     /// <summary>
     /// The base class to represent a namespace imported from a PE/module. Namespaces that differ
     /// only by casing in name are not merged.
@@ -28,6 +198,8 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols.Metadata.PE
         /// mapped by their name (case-sensitively).
         /// </summary>
         protected Dictionary<ReadOnlyMemory<char>, PENestedNamespaceSymbol> lazyNamespaces;
+
+        protected FrugalDictionary<ReadOnlyMemory<char>, PENestedNamespaceSymbol>? lazyNamespaces2;
 
         /// <summary>
         /// A map of types immediately contained within this namespace 
@@ -65,16 +237,17 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols.Metadata.PE
 
             var memberTypes = GetMemberTypesPrivate();
 
-            if (lazyNamespaces.Count == 0)
+            if (lazyNamespaces2.Value.Count == 0)
                 return StaticCast<Symbol>.From(memberTypes);
 
-            var builder = ArrayBuilder<Symbol>.GetInstance(memberTypes.Length + lazyNamespaces.Count);
+            var builder = ArrayBuilder<Symbol>.GetInstance(memberTypes.Length + lazyNamespaces2.Value.Count);
 
             builder.AddRange(memberTypes);
-            foreach (var pair in lazyNamespaces)
-            {
-                builder.Add(pair.Value);
-            }
+            lazyNamespaces2.Value.AddValuesToArrayBuilder(builder);
+            //foreach (var pair in lazyNamespaces2.Value)
+            //{
+            //    builder.Add(pair.Value);
+            //}
 
             return builder.ToImmutableAndFree();
         }
@@ -98,7 +271,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols.Metadata.PE
             PENestedNamespaceSymbol ns = null;
             ImmutableArray<PENamedTypeSymbol> t;
 
-            if (lazyNamespaces.TryGetValue(name, out ns))
+            if (lazyNamespaces2.Value.TryGetValue(name, out ns))
             {
                 if (lazyTypes.TryGetValue(name, out t))
                 {
@@ -163,6 +336,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols.Metadata.PE
         /// <returns>PEModuleSymbol containing the namespace.</returns>
         internal abstract PEModuleSymbol ContainingPEModule { get; }
 
+        [MemberNotNull(nameof(lazyNamespaces2))]
         protected abstract void EnsureAllMembersLoaded();
 
         /// <summary>
@@ -226,9 +400,9 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols.Metadata.PE
         private void LazyInitializeNamespaces(
             IEnumerable<KeyValuePair<string, IEnumerable<IGrouping<string, TypeDefinitionHandle>>>> childNamespaces)
         {
-            if (this.lazyNamespaces == null)
+            if (!this.lazyNamespaces2.HasValue)
             {
-                var namespaces = new Dictionary<ReadOnlyMemory<char>, PENestedNamespaceSymbol>(ReadOnlyMemoryOfCharComparer.Instance);
+                var namespaces = new FrugalDictionary<ReadOnlyMemory<char>, PENestedNamespaceSymbol>(ReadOnlyMemoryOfCharComparer.Instance);
 
                 foreach (var child in childNamespaces)
                 {
@@ -236,7 +410,7 @@ namespace Microsoft.CodeAnalysis.CSharp.Symbols.Metadata.PE
                     namespaces.Add(c.Name.AsMemory(), c);
                 }
 
-                Interlocked.CompareExchange(ref this.lazyNamespaces, namespaces, null);
+                Interlocked.CompareExchange(ref this.lazyNamespaces2, namespaces, null);
             }
         }
 
