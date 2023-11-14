1ff42b3ba624e1776b22bb06c5ef5cc6fce5326b
Todd Grunke <toddgrun@microsoft.com>
2023-11-14 07:09:28 -0800

    temp commit

--- src/Workspaces/Core/Portable/Shared/Utilities/SemanticMap.Walker.cs
+++ src/Workspaces/Core/Portable/Shared/Utilities/SemanticMap.Walker.cs
@@ -2,10 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Threading;
 using Microsoft.CodeAnalysis.Shared.Extensions;
+using Roslyn.Utilities;
 
 namespace Microsoft.CodeAnalysis.Shared.Utilities
 {
@@ -33,6 +32,33 @@ namespace Microsoft.CodeAnalysis.Shared.Utilities
                 }
 
                 base.VisitToken(token);
+            }
+
+            public void Visit2(SyntaxNode node)
+            {
+                foreach (var child in node.DescendantNodesAndTokensAndSelf())
+                {
+                    if (child.AsNode() is SyntaxNode childNode)
+                    {
+                        var info = semanticModel.GetSymbolInfo(childNode);
+                        if (!IsNone(info))
+                        {
+                            map._expressionToInfoMap.Add(childNode, info);
+                        }
+                    }
+                    else if (child.AsToken() is SyntaxToken childToken)
+                    {
+                        var info = semanticModel.GetSymbolInfo(childToken, cancellationToken);
+                        if (!IsNone(info))
+                        {
+                            map._tokenToInfoMap.Add(childToken, info);
+                        }
+                    }
+                    else
+                    {
+                        throw ExceptionUtilities.Unreachable();
+                    }
+                }
             }
 
             private static bool IsNone(SymbolInfo info)
